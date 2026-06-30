9e386ea9d66b924a9e99810b110871fff629d0a2
Pablo Sarró Sánchez <pablo.sarrosanchez@epfl.ch>
2026-06-30 14:05:44 +0200

    Baseline for 'About' Section

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-console.log("Website loaded!");
+function showTab(tabId) {
+    const sections = document.querySelectorAll(".tab-content");
+    sections.forEach(sec => sec.classList.remove("active"));
+
+    document.getElementById(tabId).classList.add("active");
+}
